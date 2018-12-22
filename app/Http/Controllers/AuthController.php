ce493b23495d72cada15d0ff419a40ee7b529828
Mukesh Tivari <mukesh.t@crestinfosystems.net>
2018-12-22 19:36:49 +0530

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+
+  public function login() {
+    $credentials = request(['email', 'password']);
+
+    if (!$token = auth('api')->attempt($credentials)) {
+      return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    return response()->json([
+                'token' => $token,
+                'expires' => auth('api')->factory()->getTTL() * 60,
+    ]);
+  }
+
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    DB::table('users')->insert([
+        'name' => 'Mukesh Tivari',
+        'email' => 'er.mts1993@gmail.com',
+        'password' => bcrypt('password'),
+        'role' => 'ADMIN'
+    ]);
+    DB::table('users')->insert([
+        'name' => 'Bhautik Jani',
+        'email' => 'bhautikjani@gmail.com',
+        'password' => bcrypt('password'),
+        'role' => 'USER'
+    ]);
+  }
+
+}
